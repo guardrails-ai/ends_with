5070fd60c195229c9ec6abf9c7481ffb471d9960
karan@guardrailsai.com <thekaranacharya@gmail.com>
2024-02-21 18:59:40 -0500

    Update logic

--- validator/main.py
+++ validator/main.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union, Callable
 
 from guardrails.logger import logger
 from guardrails.validator_base import (
@@ -10,33 +10,39 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/ends_with", data_type="list")
+@register_validator(name="guardrails/ends_with", data_type=["string", "list"])
 class EndsWith(Validator):
     """Validates that a list ends with a given value.
 
     **Key Properties**
 
-    | Property                      | Description                       |
-    | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `ends-with`                       |
-    | Supported data types          | `list`                            |
-    | Programmatic fix              | Append the given value to the list. |
+    | Property                      | Description                         |
+    | ----------------------------- | ---------------------------------   |
+    | Name for `format` attribute   | `ends-with`                         |
+    | Supported data types          | `string`, `list`                    |
+    | Programmatic fix              | Append the given value if absent    |
 
     Args:
         end: The required last element.
     """
 
-    def __init__(self, end: str, on_fail: str = "fix"):
-        super().__init__(on_fail=on_fail, end=end)
+    def __init__(
+        self, end: str, on_fail: Union[Callable[..., Any], None] = None, **kwargs
+    ):
+        super().__init__(on_fail=on_fail, end=end, **kwargs)
         self._end = end
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
         logger.debug(f"Validating {value} ends with {self._end}...")
 
-        if not value[-1] == self._end:
+        if value[-1] != self._end:
             return FailResult(
                 error_message=f"{value} must end with {self._end}",
-                fix_value=value + [self._end],
+                fix_value=(
+                    value.extend(self._end)
+                    if isinstance(value, list)
+                    else value + self._end
+                ),
             )
 
         return PassResult()
